381f750335541afe02920e99d738cd5524ff0225
Vikartan Sood <139503457+techdev-VIK@users.noreply.github.com>
2025-05-22 23:25:51 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        Hello
+       {/* NAVBAR */}
+       <Navbar />
+       {/* BREADCRUMB */}
+       {/* INTRODUCTION */}
+       {/* FEATURED POSTS */}
+       {/* POST LIST */}
     </div>
   )
 }
 
 export default App
-  
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 
 
 const Navbar = () => {
 
+    const [open , setOpen] = useState(false);
+
     return(
-        <>
-        
-        </>
+        <div className="w-full h-16 md:h-20 flex items-center justify-between">
+            <div className="flex items-center gap-4 text-2xl font-bold">
+                <img src="/blogger.png" alt="Blogvue" className="w-8 h-8"/>
+                <span>Blogvue</span>
+            </div>
+            
+            {/* Desktop Menu */}
+            <div className="md:hidden">
+                
+            </div>
+
+            {/* Mobile Menu */}
+            <div className="hidden md:flex">
+               <div className="cursor-pointer text-4xl" onClick={() => setOpen(prev => !prev)}>
+                {!open ? "☰" : "X"}
+               </div>
+            </div>
+        </div>
     )
 }
 
